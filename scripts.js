d89a541778bd5ef1bfe11a0a7ad1bbcbfc98f0be
zsoka90 <zsoka90@gmail.com>
2020-04-16 18:13:22 +0200

    :(
    :( thumbnails

--- scripts.js
+++ scripts.js
@@ -1,32 +1,3 @@
-let photo1 = {
-    photo: 'Images/3shilouettes.jpg',
-    title: 'Three shilouettes by the Ocean',
-    description: 'Ahead of the boardwalk I’m wandering on lies the delightfully green Magdalena peninsula; to my right, the golden sands of Playa el Puntal beach',
-};
-
-let photo2 = {
-    photo: 'Images/OceanAndGrass.jpg',
-    title: 'Ocean And Grass',
-    description: "It's not often that I'm attracted to a city so quickly, especially one with such little fanfare; indeed I knew nothing of this Spanish coastal city before my arrival",
-};
-
-let photo3 = {
-    photo: 'Images/Boats.jpg',
-    title: 'Boats',
-    description: 'This is a place I could actually live, I muse, as a local cycles past me leisurely, no doubt enjoying another glorious morning in this part of the world. ',
-};
-
-let photo4 = {
-    photo: 'Images/City.jpg',
-    title: 'City',
-    description: 'Ahead of the boardwalk I’m wandering on lies the delightfully green Magdalena peninsula; to my right, the golden sands of Playa el Puntal beach',
-};
-
-let photo5 = {
-    photo: 'Images/Surfer.jpg',
-    title: 'Surfer',
-    description: 'There are certain things we always seek out in a new city; a strong local culture, vast natural beauty, an impressive gourmet scene, and most importantly, many beautiful golden beaches, and Santander has all in spades',
-};
 
 // $('#photo').attr('src', photo1.photo);
 
@@ -34,28 +5,80 @@ let photo5 = {
 
 // $('#photo-description').text(photo1.description);
 
-let eachPhotos = [photo1, photo2, photo3, photo4, photo5];
+let eachPhotos = [
+    {
+    photo: 'Images/3shilouettes.jpg',
+    title: 'Three shilouettes by the Ocean',
+    description: 'Ahead of the boardwalk I’m wandering on lies the delightfully green Magdalena peninsula; to my right, the golden sands of Playa el Puntal beach'
+},
 
-let currentPhoto = 0;
+{
+    photo: 'Images/OceanAndGrass.jpg',
+    title: 'Ocean And Grass',
+    description: "It's not often that I'm attracted to a city so quickly, especially one with such little fanfare; indeed I knew nothing of this Spanish coastal city before my arrival",
+},
 
-$('#photo').attr('src', eachPhotos[currentPhoto].photo);
+{
+    photo: 'Images/Boats.jpg',
+    title: 'Boats',
+    description: 'This is a place I could actually live, I muse, as a local cycles past me leisurely, no doubt enjoying another glorious morning in this part of the world. ',
+},
 
-$('#photo-title').text(eachPhotos[currentPhoto].title);
+{
+    photo: 'Images/City.jpg',
+    title: 'City',
+    description: 'Ahead of the boardwalk I’m wandering on lies the delightfully green Magdalena peninsula; to my right, the golden sands of Playa el Puntal beach',
+},
 
-$('#photo-description').text(eachPhotos[currentPhoto].description);
+{
+    photo: 'Images/Surfer.jpg',
+    title: 'Surfer',
+    description: 'There are certain things we always seek out in a new city; a strong local culture, vast natural beauty, an impressive gourmet scene, and most importantly, many beautiful golden beaches, and Santander has all in spades',
+}
+
+];
+
+// let currentPhoto = 0;
+
+// $('#photo').attr('src', eachPhotos[currentPhoto].photo);
+// $('#photo-title').text(eachPhotos[currentPhoto].title);
+// $('#photo-description').text(eachPhotos[currentPhoto].description);
 
 let loadPhoto = (photoNumber) => {
     $('#photo').attr('src', eachPhotos[photoNumber].photo);
-  }
+    $('#photo-title').text(eachPhotos[photoNumber].title);
+    $('#photo-description').text(eachPhotos[photoNumber].description);
+  };
 
-  loadPhoto(currentPhoto);
-
-  $('#forward').click(() => {
-    currentPhoto++;
-    currentPhoto = currentPhoto % eachPhotos.length;
+  
+$(() => {
+    let currentPhoto = 0;
     loadPhoto(currentPhoto);
-
+  
+    $("#forward").click(() => {
+      currentPhoto++;
+      if (currentPhoto >= eachPhotos.length) {
+        currentPhoto = 0;
+      }
+      loadPhoto(currentPhoto);
+    });
+  
+    $("#back").click(() => {
+      currentPhoto--;
+      if (currentPhoto < 0) {
+        currentPhoto = eachPhotos.length - 1;
+      }
+      loadPhoto(currentPhoto);
+    });
   });
 
 
 
+// let createThumbs = $("#tnContainer").append(`<img src=${eachPhotos.photo}>`);
+// eachPhotos.forEach(element => {
+    // createThumbs(element);
+// })
+
+eachPhotos.forEach(function(item){
+  $("#tnContainer").append(`<img src=${item.photo}>`);});
+  
